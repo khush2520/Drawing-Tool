6c4031f45d3ee6e6216c95ad48fddf8a9e48c4fc
Medha Prasad <medha27prasad@gmail.com>
2024-05-05 03:49:08 +0530

    sujal fixed copy for groups, vinit's changes for background color and black lines, fixed edit button for color change, corner style functionality not added yet

--- rotate.py
+++ rotate.py
@@ -1,21 +1,7 @@
 import sys
-from PyQt6.QtCore import Qt, QPointF
-from PyQt6.QtGui import QPainter, QPen, QIcon, QBrush
-from PyQt6.QtWidgets import (
-    QWidget,
-    QSlider,
-    QHBoxLayout,
-    QVBoxLayout,
-    QPushButton,
-    QApplication,
-    QGraphicsView,
-    QGraphicsScene,
-    QGraphicsLineItem,
-    QGraphicsRectItem,
-    QGraphicsEllipseItem,
-    QGraphicsItem,
-    QGraphicsItemGroup,
-)
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
 
 
 class Shape:
@@ -25,9 +11,48 @@ class Shape:
         self.end_point = end_point
         self.color = color
 
+class EditDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        layout = QVBoxLayout()
+
+        self.color_button = QPushButton("Change Color")
+        self.color_button.clicked.connect(self.chooseColor)
+        layout.addWidget(self.color_button)
+
+        self.corner_button = None
+        for item in parent.scene.selectedItems():
+            if isinstance(item, QGraphicsRectItem):
+                self.corner_combo = QComboBox()
+                self.corner_combo.addItems(["Select Corner Style", "Sharp", "Curved"])
+                self.corner_combo.currentIndexChanged.connect(self.handleCornerStyleChange)
+                layout.addWidget(self.corner_combo)
+                break
+
+        self.setLayout(layout)
+
+    def chooseColor(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            items = self.parent().scene.selectedItems()
+            for item in items:
+                if item.isSelected():
+                    pen = item.pen()
+                    pen.setColor(color)
+                    item.setPen(pen)
+
+    def handleCornerStyleChange(self, index):
+        if index == 1:  # Sharp
+            self.corner_style = "Sharp"
+        elif index == 2:  # Curved
+            self.corner_style = "Curved"
+        else:
+            self.corner_style = None
+
 class GraphicsScene(QGraphicsScene):
     def __init__(self):
         super().__init__(0, 0, 400, 400)
+        self.setBackgroundBrush(Qt.GlobalColor.white)
         self.startPoint = None
         self.endPoint = None
         self.drawingShape = None
@@ -119,8 +144,8 @@ class GraphicsScene(QGraphicsScene):
                                          self.startPoint.y(),
                                          self.endPoint.x(),
                                          self.endPoint.y())
-                line_pen = QPen(Qt.GlobalColor.cyan)
-                line_pen.setWidth(10)
+                line_pen = QPen(Qt.GlobalColor.black)
+                line_pen.setWidth(2)
                 line.setPen(line_pen)
                 self.addItem(line)
                 line.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
@@ -130,10 +155,8 @@ class GraphicsScene(QGraphicsScene):
                                          shape.start_point.y(),
                                          abs(shape.end_point.x() - shape.start_point.x()),
                                          abs(shape.end_point.y() - shape.start_point.y()))
-                rect_brush = QBrush(Qt.GlobalColor.red)
-                rect.setBrush(rect_brush)
-                rect_pen = QPen(Qt.GlobalColor.cyan)
-                rect_pen.setWidth(10)
+                rect_pen = QPen(Qt.GlobalColor.black)
+                rect_pen.setWidth(2)
                 rect.setPen(rect_pen)
                 self.addItem(rect)
                 rect.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
@@ -194,7 +217,7 @@ class MainWindow(QWidget):
         group_button = QPushButton("Group")
         group_button.clicked.connect(self.groupSelectedShapes)
         vbox.addWidget(group_button)
-        
+
         ungroup_button = QPushButton("Ungroup")
         ungroup_button.clicked.connect(self.ungroupSelectedShapes)
         vbox.addWidget(ungroup_button)
@@ -211,6 +234,10 @@ class MainWindow(QWidget):
         delete_button = QPushButton("Delete")
         delete_button.clicked.connect(self.deleteSelectedShape)
         vbox.addWidget(delete_button)
+
+        edit_button = QPushButton("Edit")
+        edit_button.clicked.connect(self.edit)
+        vbox.addWidget(edit_button)
 
         view = QGraphicsView(self.scene)
         view.setRenderHint(QPainter.RenderHint.Antialiasing)
@@ -261,6 +288,36 @@ class MainWindow(QWidget):
                 new_rect.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
                 new_rect.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
                 self.scene.addItem(new_rect)
+            elif isinstance(item, QGraphicsItemGroup):
+                group = item
+                new_group = QGraphicsItemGroup()
+
+                for child in group.childItems():
+                    if isinstance(child, QGraphicsLineItem):
+                        line = child
+                        new_line = QGraphicsLineItem(line.line().x1(), line.line().y1(), line.line().x2(), line.line().y2())
+                        new_line.setPen(line.pen())
+                        new_line.setPos(line.pos())
+                        new_group.addToGroup(new_line)
+                    elif isinstance(child, QGraphicsRectItem):
+                        rect = child
+                        new_rect = QGraphicsRectItem(rect.rect())
+                        new_rect.setBrush(rect.brush())
+                        new_rect.setPen(rect.pen())
+                        new_rect.setPos(rect.pos())
+                        new_group.addToGroup(new_rect)
+                    elif isinstance(child, QGraphicsEllipseItem):
+                        ellipse = child
+                        new_ellipse = QGraphicsEllipseItem(ellipse.rect())
+                        new_ellipse.setBrush(ellipse.brush())
+                        new_ellipse.setPen(ellipse.pen())
+                        new_ellipse.setPos(ellipse.pos())
+                        new_group.addToGroup(new_ellipse)
+
+                new_group.setPos(group.pos() + QPointF(20, 20))
+                new_group.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
+                new_group.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
+                self.scene.addItem(new_group)
             # elif isinstance(item, QGraphicsEllipseItem):
             #     ellipse = item
             #     new_ellipse = QGraphicsEllipseItem(ellipse.rect())
@@ -293,14 +350,40 @@ class MainWindow(QWidget):
             if isinstance(item, QGraphicsItemGroup):
                 group = item
                 self.scene.removeItem(group)
-                group_pos = group.pos()  
+                group_pos = group.pos()
                 for child in group.childItems():
-                    child_pos = child.pos()  
-                    ungrouped_pos = group_pos + child_pos  
+                    child_pos = child.pos()
+                    ungrouped_pos = group_pos + child_pos
                     self.scene.addItem(child)
-                    child.setPos(ungrouped_pos)  
+                    child.setPos(ungrouped_pos)
                     child.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
                     child.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
+
+    def edit(self):
+        items = self.scene.selectedItems()
+        if not items:
+            QMessageBox.warning(self, "No Selection", "No item selected.")
+            return
+
+        dialog = EditDialog(self)
+        dialog.exec()
+
+        for item in items:
+            if item.isSelected():
+                pen = item.pen()
+                # if dialog.corner_style:
+                #     if isinstance(item, QGraphicsRectItem):
+                #         if dialog.corner_style == "Sharp":
+                #             pass
+
+                #         elif dialog.corner_style == "Curved":
+                #             pass
+
+
+                if dialog.color_button.isChecked():
+                    color = dialog.color_button.palette().color(dialog.color_button.backgroundRole())
+                    pen.setColor(color)
+                item.setPen(pen)
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
